e8590592acc5cd47766a963033b9486e4447c1de
Kittana575 <kittana.tean@bumail.net>
2025-03-06 10:31:32 +0700

    update

--- Assets/Script/Airplane.cs
+++ Assets/Script/Airplane.cs
@@ -19,8 +19,18 @@ public class Airplane : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+
+        Vector3 lift = Vector3.Project(rb.linearVelocity,transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+        rb.linearDamping = rb.linearVelocity.magnitude * drag;
+        rb.angularDamping = rb.linearVelocity.magnitude * angularDrag;
+        
         
     }
 }
