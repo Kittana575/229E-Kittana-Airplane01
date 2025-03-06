0b47fb9acab8c03b8ed97db1e7e0e95040fb14ad
Kittana575 <kittana.tean@bumail.net>
2025-03-06 10:14:40 +0700

    Update_Code

--- Assets/Script/Airplane.cs
+++ Assets/Script/Airplane.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class Airplane : MonoBehaviour
 {
 
-
-
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
 
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
